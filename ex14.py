55c85fe97d5ea4a1abe22be5cc589775eeb8f48b
evalle <shmarnev@gmail.com>
2015-06-30 17:08:45 +0200

    Add Exercise 14 from 'learn Python the Hard way' book

--- ex14.py
+++ ex14.py
@@ -1,9 +1,9 @@
 from sys import argv
 
-script, user_name = argv
+script, user_name, date = argv
 prompt = '> '
 
-print "Hi %s, I'm the %s script." % (user_name, script)
+print "Hi %s, I'm the %s script and today is %r" % (user_name, script, date)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
