25540de82696f5d45c32a291c4582af9ebd23227
evalle <shmarnev@gmail.com>
2015-07-05 13:45:54 +0200

    Add '+=' instead of current_line = current_line + 1

--- ex20.py
+++ ex20.py
@@ -9,7 +9,7 @@ def rewind (f):
     f.seek(0)
 
 def print_a_line(line_count, f):
-    print line_count, f.readline()
+    print line_count, f.readline(),
 
 current_file = open(input_file)
 
@@ -25,9 +25,9 @@ print "Let's print three lines:"
 
 current_line = 1
 print_a_line(current_line, current_file)
-
-current_line = current_line + 1
+rewind(current_file)
+current_line += 1
 print_a_line(current_line, current_file)
-
-current_line = current_line + 1
+rewind(current_file)
+current_line += 1
 print_a_line(current_line, current_file)
