e7cc8ae955c7d28e33b3b3f2300b75d90f60ea57
evalle <shmarnev@gmail.com>
2015-07-13 17:09:23 +0200

    Fix some errors in ex26

--- ex26.py
+++ ex26.py
@@ -7,7 +7,7 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
@@ -42,7 +42,7 @@ poem = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
-nor comprehend passion from intuition\n
+nor comprehend passion from intuition
 and requires an explantion\n
 \t\twhere there is none.
 """
@@ -71,22 +71,22 @@ print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_pont)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 
 
 sentence = "All good things come to those who wait."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 print sorted_words
 
 print_first_and_last(sentence)
 
-print_first_and_last_sorted(senence)
+print_first_and_last_sorted(sentence)
 
