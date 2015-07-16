a427e71667ec0dfd88823d4d6da3c46d047cf58a
evalle <shmarnev@gmail.com>
2015-07-16 11:58:21 +0200

    Create new variable for increment

--- ex33.py
+++ ex33.py
@@ -1,10 +1,11 @@
-def loop(digit):
+def loop(digit, inc):
     i = 0
     numbers = []
     while i < digit:
         numbers.append(i)
-        i = i + 1
+        i = i + inc
         print "Numbers now: ", numbers
 
-digit = int(raw_input('> '))
-loop(digit)
+digit = int(raw_input('> Enter digit for while loop: '))
+inc = int(raw_input('> Enter incremental digit: '))
+loop(digit, inc)
