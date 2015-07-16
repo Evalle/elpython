6757bc55d3bbd36ff85dec37c6915c6d58fef5af
evalle <shmarnev@gmail.com>
2015-07-16 10:06:03 +0200

    Clean up ex33

--- ex33.py
+++ ex33.py
@@ -1,20 +1,10 @@
-i = 0
-numbers = []
-
 def loop(digit):
     i = 0
     numbers = []
     while i < digit:
-        print "At the top i is %d" % i
         numbers.append(i)
         i = i + 1
         print "Numbers now: ", numbers
-        print "At the bottom i is %d" % i
-        print "The numbers: ", numbers
-
-#for num in numbers:
-#    print num
-
 
 digit = int(raw_input('> '))
 loop(digit)
