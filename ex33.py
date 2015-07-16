c3b8036d8888972ffba31289bd6c27bb1f8eb589
evalle <shmarnev@gmail.com>
2015-07-16 07:12:57 +0200

    Addfunction

--- ex33.py
+++ ex33.py
@@ -1,15 +1,20 @@
 i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def loop(digit):
+    i = 0
+    numbers = []
+    while i < digit:
+        print "At the top i is %d" % i
+        numbers.append(i)
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+        print "The numbers: ", numbers
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+#for num in numbers:
+#    print num
 
-print "The numbers: "
 
-for num in numbers:
-    print num
+digit = int(raw_input('> '))
+loop(digit)
