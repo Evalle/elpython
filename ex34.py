84d53f2690e86a76ccf8f7c0735f237798514c17
evalle <shmarnev@gmail.com>
2015-07-18 13:18:13 +0200

    Rename + add first function

--- ex34.py
+++
@@ -1,4 +0,0 @@
-from sys import exit()
-
-def gold_room():
-    print "This room is full of cold. How much do you take?"
---
+++ ex35.py
@@ -0,0 +1,20 @@
+from sys import exit()
+
+def gold_room():
+    print "This room is full of cold. How much do you take?"
+
+    choice = raw_input("> ")
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number.")
+
+    if how_much < 50:
+        print "Nice, you're not greedy, you win!"
+        exit(0)
+    else:
+        dead("You greedy bastard!")
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
