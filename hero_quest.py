faa915151a5b56134bf18194f36ba3f4ef5708f7
evalle <shmarnev@gmail.com>
2015-07-19 12:05:32 +0200

    Add some function, 1st prototype of the game

--- hero_quest.py
+++ hero_quest.py
@@ -1,2 +1,35 @@
-# Here will be a some adventure game
+from sys import exit
+import string
 
+def start():
+    print '''
+    You're in the middle of the square room.
+    You can see two doors.
+    One in the left and another one in the right from you.
+    Which one you will choose?
+    '''
+    choice = raw_input('> Choose your door (left or right): ')
+    if "left" in choice:
+        gold_room()
+    elif "right" in choice:
+        lab_room()
+    else:
+        print "Please choose your door"
+        start()
+
+def dead():
+    exit(0)
+
+def gold_room():
+    print '''
+    You're in the gold room
+    '''
+    dead()
+
+def lab_room():
+    print '''
+    You're in the lab_room
+    '''
+    dead()
+
+start()
