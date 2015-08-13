d779185e476712116cf520908a6d9ecea1b9d5fa
evalle <shmarnev@gmail.com>
2015-08-13 17:52:06 +0200

    Add another class
    Signed-off-by: evalle <shmarnev@gmail.com>

--- GFPP/main.py
+++ GFPP/main.py
@@ -7,14 +7,22 @@ class Scene(object):
     def enter(self):
         print "This scene is not yet configured. Subclass it and implement enter()."
         exit(1)
-        
+
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene_enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        # be sure to print out the last scene
+        current_scene.enter()
 
 class Map(object):
 
