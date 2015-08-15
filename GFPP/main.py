9343c5e81be418fc1755a7a25ae20d5ed1710a38
evalle <shmarnev@gmail.com>
2015-08-15 17:21:15 +0200

    Add Death class
    Signed-off-by: evalle <shmarnev@gmail.com>

--- GFPP/main.py
+++ GFPP/main.py
@@ -37,9 +37,16 @@ class Map(object):
 
 
 class Death(Scene):
+    quips = [
+        "You died. You kinda suck at his.",
+        "Your mom would be proud... if she were smarter.",
+        "Such a looser.",
+        "I have a small puppy that's better at this."
+    ]
 
     def enter(self):
-        pass
+        print Death.quips[randint(0, len(self.quips)-1)]
+        exit(1)
 
 class CentralCorridor(Scene):
 
