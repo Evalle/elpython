3f0166ba04186455fb279823ed1eefa0748afaba
evalle <shmarnev@gmail.com>
2015-08-19 15:27:50 +0200

    Fix an error with 'current_scene.enter()'
    Signed-off-by: evalle <shmarnev@gmail.com>

--- GFPP/main.py
+++ GFPP/main.py
@@ -18,7 +18,7 @@ class Engine(object):
         last_scene = self.scene_map.next_scene('finished')
 
         while current_scene != last_scene:
-            next_scene_name = current_scene_enter()
+            next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
 
         # be sure to print out the last scene
