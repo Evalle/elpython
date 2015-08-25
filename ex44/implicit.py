9841ed3d8e1c52f00df78a53dab37dc71453edc6
evalle <shmarnev@gmail.com>
2015-08-25 16:41:09 +0200

    Add implicit.py script
    Signed-off-by: evalle <shmarnev@gmail.com>

---
+++ ex44/implicit.py
@@ -0,0 +1,15 @@
+# implicit inheritance
+
+class Teacher(object):
+    
+    def function(self):
+        print "Teacher's function()"
+
+class Student(Teacher):
+    pass
+
+teacher = Teacher()
+student = Srudent()
+
+teacher.fucntion()
+student.function()
