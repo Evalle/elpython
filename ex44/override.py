cb9f5a03b27aec49661105618417e80afb998e25
evalle <shmarnev@gmail.com>
2015-08-25 20:04:13 +0200

    override.py
    Signed-off-by: evalle <shmarnev@gmail.com>

--- ex44/override.py
+++ ex44/override.py
@@ -9,8 +9,15 @@ class Children(Parent):
     def function(self):
         print "Children's function()"
 
+    def multiply(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
+        print first_num * second_num
+
+
 parent = Parent()
 son = Children()
 
 parent.function()
 son.function()
+son.multiply(2,2)
