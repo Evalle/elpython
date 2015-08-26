c33bdb3953d98ea465a59ebb22c5c804819aae94
evalle <shmarnev@gmail.com>
2015-08-26 15:04:13 +0200

    Add example of composition

---
+++ ex44/composition.py
@@ -0,0 +1,34 @@
+# example of the composition
+
+class Other(object):
+
+    def override(self):
+        print "OTHER override()"
+
+    def implicit(self):
+        print "OTHER implicit"
+
+    def altered(self):
+        print "OTHER altered"
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print "CHILD override()"
+
+    def altered(self):
+        print "CHILD before altered()"
+        self.other.altered()
+        print "CHILD after altered()"
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
