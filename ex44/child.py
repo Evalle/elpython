a4a4feb8c8b8ae381781e14a26c717b1885f9eee
evalle <shmarnev@gmail.com>
2015-08-26 15:47:28 +0200

    Add example for composition

---
+++ ex44/child.py
@@ -0,0 +1,26 @@
+# example of using composition to package code into modules
+# 2. Child
+
+from other import Other
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print "CHILD override"
+
+    def altered(self):
+        print "CHILD before altered"
+        self.other.altered()
+        print "CHILD after altered"
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
---
+++ ex44/other.py
@@ -0,0 +1,13 @@
+# example for using compostion for packaging code into modules
+# basic module Other
+
+class Other(object):
+
+    def implicit(self):
+        print "OTHER implicit"
+
+    def override(self):
+        print "OTHER override"
+
+    def altered(self):
+        print "OTHER altered"
