6456b6fbaf5d1aca48cf3244c7e8b230ee26435d
evalle <shmarnev@gmail.com>
2015-09-29 08:04:08 +0200

    Add another unittest

--- ex47/tests/ex47_tests.py
+++ ex47/tests/ex47_tests.py
@@ -9,7 +9,6 @@ def test_room():
     assert_equal(gold.paths, {})
 
 def test_room_paths():
-
     center = Room("Center", "Test room in the center.")
     north = Room("North", "Test room in the north.")
     south = Room("South", "Test room in the south.")
@@ -17,3 +16,13 @@ def test_room_paths():
     center.add_paths({'north': north, 'south': south})
     assert_equal(center.go('north'), north)
     assert_equal(center.go('south'), south)
+
+def test_map():
+    start = Room("Start", "You can go west and down a hole.")
+    west = Room("Trees", "There are trees here, you can go est.")
+    down = Room("Dungeon", "It's a dark down here, you can go up.")
+
+    start.add_paths({'west': west, 'down', down})
+    west.add_paths({'east': start})
+    down.add_paths({'up': start})
+
