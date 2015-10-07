f4d0fa01859033ecdcabeada04200812a69c8822
evalle <shmarnev@gmail.com>
2015-10-07 09:59:56 +0200

    Add 2 unit tests

--- gothonweb/tests/app_tests.py
+++ gothonweb/tests/app_tests.py
@@ -1,1 +1,12 @@
-# here will be some code soon
+from nose.tools import *
+from bin.app import app
+from tests.tools import assert_reponse
+
+def test_index():
+    # check that we get a 404 on the / URL
+    resp = app.request("/")
+    assert_repsonse(resp, status="404")
+
+    #test our first GET request to /hello
+    resp = app.request("/hello")
+    assert_response(resp)
